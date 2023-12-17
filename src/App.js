fec96f7caf9c2682525ab6672d3f4165a4b9a270
jss5794 <jss5794@penta.co.kr>
2023-12-17 22:32:09 +0900

    리액트 200제

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import SpreadOperator from "./R011_SpreadOperator";
+import Jquery from "./R016_Jquery";
 
 function App() {
   return (
    <div>
        <h1>Start React 200!</h1>
        <p>CSS 적용하기</p>
-       <SpreadOperator />
+       <Jquery />
    </div>
   );
 }
